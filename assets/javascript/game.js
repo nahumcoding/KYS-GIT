d7062d6c3344f4272f3684a8ed40e875ecdf3145
Angel <Angelj@Lauras-MacBook-Pro.local>
2018-03-19 19:22:55 -0400

    1st refresh screen

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var spotifyResponse = [artist1, artist2, artist3];
+
+
+$(document).ready(function() { 
+
+var spotifyApiUrl='https://accounts.spotify.com/authorize?client_id=117298f8d65647da97d993cc58cfd26d&redirect_uri=https://www.google.com&scope=user-read-private%20user-read-email&response_type=token&state=123'
+	
+$("#singlebutton").click(function(){
+		
+
+		});
+
+});
+
+
+// functions that pull artists/concerts and allows to refresh the screen
+
+function updateConcerts();
+
+
+function refreshScreen(){
+	$('')
+};
+
+
+
+// for firebasde we will store artists
+
+// present on saturday
+
+
+// 2 functions are needed.  Get function will nbe in "API.js' Trigger call to API and call to do refresh
+
+// refreshing the DOM. Refreshing the page 
